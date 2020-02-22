b5177835a74b5864c42ed5178950a4cefdbee348
yogain19 <yogendrasaxena56@gmail.com>
2020-02-22 14:44:45 +0530

    Converted Class to Hooks for Login Comp

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,111 +1,107 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { LoginAction } from "../redux/Actions/LoginAction";
 import { connect } from "react-redux";
 import qp from "query-parse";
+function Login(props) {
+  const [submit, setSubmit] = useState("Submit");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const emailRef = React.createRef();
 
-class Login extends Component {
-  state = { submit: "Submit", mail: "", password: "" };
-  emailRef = React.createRef();
-
-  async onSubmit(event) {
+  const onSubmit = event => {
     event.preventDefault();
-    this.setState({ submit: <div class="spinner-border"></div> });
-    let query = qp.toObject(this.props.location.search.substring(1));
-    let pathing = query.preserved || `/home?email=${this.state.email}`;
-    this.props.LoginAction(this.state.email, this.state.password, response => {
-      this.props.history.push(pathing);
+    setSubmit(<div class="spinner-border"></div>);
+    let query = qp.toObject(props.location.search.substring(1));
+    let pathing = query.preserved || `/home?email=${email}`;
+    props.LoginAction(email, password, response => {
+      props.history.push(pathing);
     });
-  }
+  };
 
-  componentDidMount() {
-    this.emailRef.current.focus();
-    if (this.props.LoginDetails.isLoggedIn) {
-      this.props.history.push("/dashboard");
+  useEffect(() => {
+    emailRef.current.focus();
+    if (props.LoginDetails.isLoggedIn) {
+      props.history.push("/dashboard");
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <div id="login">
-        <h3 className="text-center text-info pt-5">Login</h3>
-        <div className="container">
-          <div
-            id="login-row"
-            className="row justify-content-center align-items-center"
-          >
-            <div id="login-column" className="col-md-6">
-              <div id="login-box" className="col-md-12">
-                <form id="login-form" className="form">
-                  <div className="form-group">
-                    <label htmlFor="username" className="text-info">
-                      Email:
-                    </label>
-                    <br />
-                    <input
-                      ref={this.emailRef}
-                      type="text"
-                      name="username"
-                      id="username"
-                      className="form-control"
-                      onChange={event =>
-                        this.setState({ email: event.target.value })
-                      }
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="password" className="text-info">
-                      Password:
-                    </label>
-                    <br />
-                    <input
-                      type="text"
-                      name="password"
-                      id="password"
-                      className="form-control"
-                      onChange={event =>
-                        this.setState({ password: event.target.value })
-                      }
-                    />
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="remember-me" className="text-info">
-                      <span>Remember me</span> 
-                      <span>
-                        <input
-                          id="remember-me"
-                          name="remember-me"
-                          type="checkbox"
-                        />
-                      </span>
-                    </label>
-                    <br />
-                    <button
-                      type="submit"
-                      name="submit"
-                      className="btn btn-info btn-md"
-                      value={this.state.submit}
-                      onClick={event => this.onSubmit(event)}
-                    >
-                      {this.state.submit}
-                    </button>
-                  </div>
-                  <div id="register-link" className="text-right">
-                    <Link to="/Register" className="text-info">
-                      Register here
-                    </Link>
-                  </div>
-                </form>
-              </div>
+  return (
+    <div id="login">
+      <h3 className="text-center text-info pt-5">Login</h3>
+      <div className="container">
+        <div
+          id="login-row"
+          className="row justify-content-center align-items-center"
+        >
+          <div id="login-column" className="col-md-6">
+            <div id="login-box" className="col-md-12">
+              <form id="login-form" className="form">
+                <div className="form-group">
+                  <label htmlFor="username" className="text-info">
+                    Email:
+                  </label>
+                  <br />
+                  <input
+                    ref={emailRef}
+                    type="text"
+                    name="username"
+                    id="username"
+                    className="form-control"
+                    onChange={event => setEmail(event.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password" className="text-info">
+                    Password:
+                  </label>
+                  <br />
+                  <input
+                    type="text"
+                    name="password"
+                    id="password"
+                    className="form-control"
+                    onChange={event => setPassword(event.target.value)}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="remember-me" className="text-info">
+                    <span>Remember me</span> 
+                    <span>
+                      <input
+                        id="remember-me"
+                        name="remember-me"
+                        type="checkbox"
+                      />
+                    </span>
+                  </label>
+                  <br />
+                  <button
+                    type="submit"
+                    name="submit"
+                    className="btn btn-info btn-md"
+                    value={submit}
+                    onClick={event => onSubmit(event)}
+                  >
+                    {submit}
+                  </button>
+                </div>
+                <div id="register-link" className="text-right">
+                  <Link to="/Register" className="text-info">
+                    Register here
+                  </Link>
+                </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
-function mapStateToProps(states) {
+function mapStateToProps(states, ownProps) {
+  console.log({ states, ownProps });
   return {
     LoginDetails: states.LoginReducer
   };
